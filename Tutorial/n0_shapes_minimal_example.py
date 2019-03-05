7be6050fd1591eedda12575d0b4f8749eb093a70
kolibril13 <jan-hendrik.mueller@gmx.net>
2019-03-05 18:50:02 +0100

    # before experimenting with the histogram option

--- Tutorial/n0_shapes_minimal_example.py
+++ Tutorial/n0_shapes_minimal_example.py
@@ -1,12 +1,12 @@
 from big_ol_pile_of_manim_imports import *
 
-class Shapes(Scene):
+class EasyScence(Scene):
     def construct(self):
         dot = Dot()
         self.add(dot)
         self.wait(2)
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     module_name = os.path.basename(__file__)
-    command = "python3.7 -m manim  -a -pl  " + module_name
-    os.system(command) 
+    command = "python3.7 -m manim -pl -a " + module_name
+    os.system(command)
